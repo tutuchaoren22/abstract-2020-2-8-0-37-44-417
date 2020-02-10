c8e393c7696c7d6d29d2586659993c2e23d47768
duyuqian <15738381799@163.com>
2020-02-10 21:57:37 +0800

    [duyuqian] feat: create the chicken class as the super class of Hock&Hen&ChildChicken

--- abstract/src/main/java/com/Chicken.java
+++ abstract/src/main/java/com/Chicken.java
@@ -1,4 +1,30 @@
 package com;
 
 public class Chicken {
+    private String type;
+    private double price;
+
+    public Chicken() {
+    }
+
+    public Chicken(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
